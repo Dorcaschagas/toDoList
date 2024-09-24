c71cdc1e236c192ada78e7f376d58015f15f187f
Dorcas Chagas <128332474+Dorcaschagas@users.noreply.github.com>
2024-09-24 00:49:05 -0300

    Atualizar o main.dart

--- to_do_list/lib/main.dart
+++ to_do_list/lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'To-Do List',
       theme: ThemeData(
        
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
